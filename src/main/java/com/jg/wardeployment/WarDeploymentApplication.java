7bad2a15df1ce769eff23c723facefd0502dd6dc
Joseph Grech <joseph.grech@ixaris.com>
2020-01-24 14:50:14 +0100

    Init.

---
+++ src/main/java/com/jg/wardeployment/WarDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.jg.wardeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jg/wardeployment/WarDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.wardeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
