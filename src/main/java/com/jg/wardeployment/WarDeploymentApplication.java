3b1d8427cf1164f293c89cf8729c0bf6b7bd815c
Joseph Grech <joseph.grech@ixaris.com>
2020-01-24 15:23:45 +0100

    Applied changes.

--- src/main/java/com/jg/wardeployment/WarDeploymentApplication.java
+++ src/main/java/com/jg/wardeployment/WarDeploymentApplication.java
@@ -1,13 +1,9 @@
 package com.jg.wardeployment;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarDeploymentApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WarDeploymentApplication.class, args);
-	}
+public class WarDeploymentApplication extends SpringBootServletInitializer {
 
 }
